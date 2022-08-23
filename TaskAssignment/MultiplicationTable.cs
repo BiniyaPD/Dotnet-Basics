38356c6ff3835373a955a2242e03c523abae113d
BiniyaPD <110925626+BiniyaPD@users.noreply.github.com>
2022-08-23 11:43:42 +0530

    Add files via upload

---
+++ TaskAssignment/MultiplicationTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskAssignment
+{
+    public class MultiplicationTable
+    {
+        public void Main()
+        {
+            var task1 = Task.Factory.StartNew(() => PrintTable(5, 100));
+
+            Console.WriteLine("press any key exit");
+            Console.ReadLine();
+        }
+        public void PrintTable(int num,int sleepTime)
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.Write($"{j}*{i}={j*i}\t");
+                    Thread.Sleep(sleepTime);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TaskAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MultiplicationTable().Main();
+        }
+    }
+    }
