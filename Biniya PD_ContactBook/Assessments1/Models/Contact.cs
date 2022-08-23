bea9811300872c3904db466e89215c19e08b19cb
BiniyaPD <110925626+BiniyaPD@users.noreply.github.com>
2022-08-23 11:38:26 +0530

    Add files via upload

---
+++ Biniya PD_ContactBook/Assessments1/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment1.Models
+{
+    public class Contact
+    {
+        public string ContactName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
